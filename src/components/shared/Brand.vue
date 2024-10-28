fadbb124466b29e274a5c1afed4783653fa5c64b
Bagas Miftahun <bagasmiftahunaim@gmail.com>
2024-10-28 12:47:43 +0700

    fix: 🐛 change brand global and by pages

--- src/components/shared/Brand.vue
+++ src/components/shared/Brand.vue
@@ -1,167 +1,169 @@
 <template>
   <!-- Brand area start -->
-  <section class="main-brand__area gray-bg section-space-bottom-2">
+  <section class="main-brand__area section-space-bottom">
     <div class="brand__area">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <swiper class="brand__active wow fadeIn" data-wow-delay=".3s" :slides-per-view="5" :space-between="30" :loop="true">
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".1s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://segment.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand1.png"
-                        alt="image not found"
-                    /></a>
+            <swiper class="brand__active wow fadeIn" data-wow-delay=".2s" :slides-per-view="5" :space-between="30" :loop="true">
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".3s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://segment.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-1.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".2s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.splunk.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand2.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".4s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.splunk.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-2.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".3s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.hubspot.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand3.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".5s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.hubspot.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-3.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".4s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://app.asana.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand4.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".6s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://app.asana.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-4.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".5s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.airtasker.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand5.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".7s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.airtasker.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-5.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".1s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://segment.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand1.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".3s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://segment.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-1.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".2s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.splunk.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand2.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".4s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.splunk.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-2.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".3s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.hubspot.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand3.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".5s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.hubspot.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-3.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".4s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://app.asana.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand4.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".6s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://app.asana.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-4.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide>
-                <div
-                  class="brand__item text-center wow fadeIn animated"
-                  data-wow-delay=".5s"
-                >
-                  <div class="brand__thumb">
-                    <a href="https://www.airtasker.com/"
-                      ><img
-                        class="img-fluid"
-                        src="/assets/imgs/brands/black-brands/black-brand5.png"
-                        alt="image not found"
-                    /></a>
+                </swiper-slide>
+                <swiper-slide>
+                  <div
+                    class="brand__item text-center wow fadeIn animated"
+                    data-wow-delay=".7s"
+                  >
+                    <div class="brand__thumb">
+                      <a href="https://www.airtasker.com/"
+                        ><img
+                          class="img-fluid"
+                          src="/assets/imgs/brands/home2-companey-brands-image-5.png"
+                          alt="image not found"
+                      /></a>
+                    </div>
                   </div>
-                </div>
-              </swiper-slide>
+                </swiper-slide>
             </swiper>
           </div>
         </div>
       </div>
     </div>
   </section>
+  <!-- Brand area end -->
 </template>
 
 <script>
